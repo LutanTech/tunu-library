596e03c420c3d5ce397505f8094d0b9600005075
LutanTech <lutancorpinfoteam@gmail.com>
2026-08-27 17:12:26 +0300

    Comment out the app.run line in app.py to disable debug mode on startup

--- app.py
+++ app.py
@@ -636,4 +636,4 @@ def init_db():
 
 if __name__=="__main__":
     with app.app_context():db.create_all()
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    # app.run(debug=True, port=5000, host='0.0.0.0')
